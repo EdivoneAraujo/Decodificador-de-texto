2147e02ae1a1655b3c7098f49b63b4d3b464f17a
EdivoneAraujo <edhyvone2015@gmail.com>
2024-07-21 15:17:08 -0300

    Update script.js
    Atualizando funções dos botões

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-document.getElementById('encrypt-btn').addEventListener('click', encryptText);
-document.getElementById('decrypt-btn').addEventListener('click', decryptText);
-document.getElementById('copy-btn').addEventListener('click', copyText);
+document.querySelctor('encrypt-btn').addEventListener('click', encryptText);
+document.querySelctor('decrypt-btn').addEventListener('click', decryptText);
+document.querySelctor('copy-btn').addEventListener('click', copyText);
 
 function encryptText() {
-    // Função para criptografar o texto
     let inputText = document.getElementById('input-text').value;
     let encryptedText = inputText
         .replace(/e/g, "enter")
@@ -11,12 +10,10 @@ function encryptText() {
         .replace(/a/g, "ai")
         .replace(/o/g, "ober")
         .replace(/u/g, "ufat");
-        document.getElementById('output-text').value = encryptedText;
-    }
-
+    document.getElementById('output-text').value = encryptedText;
+}
 
 function decryptText() {
-    // Função para descriptografar o texto
     let inputText = document.getElementById('input-text').value;
     let decryptedText = inputText
         .replace(/enter/g, "e")
@@ -24,14 +21,12 @@ function decryptText() {
         .replace(/ai/g, "a")
         .replace(/ober/g, "o")
         .replace(/ufat/g, "u");
-        document.getElementById('output-text').value = decryptedText;
-    }
-
+    document.getElementById('output-text').value = decryptedText;
+}
 
 function copyText() {
-    // Função para copiar o texto para a área de transferência
     let outputText = document.getElementById('output-text');
     outputText.select();
     document.execCommand('copy');
-        alert('Texto copiado para a área de transferência!');
-    }
+    alert('Texto copiado para a área de transferência!');
+}
